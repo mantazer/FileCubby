452e0aaab59fcd180b585b52fbe547cd6e2f4927
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-10-04 18:30:45 -0400

    basic jade and stylus

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var stylus = require('stylus');
+var nib = require('nib')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +13,20 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(stylus.middleware(
+  { src: __dirname + '/public',
+    compile: compile
+  }
+));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -27,33 +41,29 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+// dev error handler: stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+    message: err.message,
+    error: err
     });
+  });
+}
+
+// production error handler: no stacktrace
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
