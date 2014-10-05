0806764e737409294dd5575fe78efed0bd7b4b6a
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-10-05 08:08:40 -0400

    renamed file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var formidable = require('formidable');
+var fs = require('fs');
 var path = require('path');
 var util = require('util');
 
 var router = express.Router();
 
+var uploadDir = __dirname + '/../tmp'
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'FileCubby' });
@@ -16,10 +18,13 @@ router.post('/upload', function(req, res) {
     keepExtensions: true
   });
     form.parse(req, function(err, fields, files) {
+      fs.rename(files.file.path, uploadDir + '/' + files.file.name, function(err) {
+        console.log('Unable to rename file', err);
+      });
       res.send(200);
+      // res.writeHead({'asd': 'asd'});
       // res.write('received upload:\n\n');
       // res.end(util.inspect({fields: fields, files: files}));
-      // console.log(files.file.path);
     });
 });
 
