c84bcd02fa0b1ec298699330e1423da9453ecd1d
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-10-05 00:59:36 -0400

    file handling, templating...

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,34 @@
 var express = require('express');
-var router = express.Router();
 var formidable = require('formidable');
+var path = require('path');
 var util = require('util');
+
+var router = express.Router();
+
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'FileCubby' });
 });
 
 router.post('/upload', function(req, res) {
-  var form = new formidable.IncomingForm();
+  var form = new formidable.IncomingForm({ 
+    uploadDir: __dirname + '/../tmp',
+    keepExtensions: true
+  });
     form.parse(req, function(err, fields, files) {
-      console.log(files.file.path);
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
+      res.send(200);
+      // res.write('received upload:\n\n');
+      // res.end(util.inspect({fields: fields, files: files}));
+      // console.log(files.file.path);
     });
 });
 
 router.get('/download', function(req, res) {
   console.log(req.param('code'));
   var code = req.param('code');
-  res.sendfile(code)
+  var filePath = path.resolve(__dirname, '../tmp/' + code);
+  console.log(filePath);
+  res.sendFile(filePath);
 });
 
 module.exports = router;
